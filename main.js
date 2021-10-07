2d0b178890ac27325ba5b0bd2027fa8dbccda00b
alexismagnin <alexis.magnin@gmail.com>
2021-10-07 13:54:07 -0300

    commit -all

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const app = Vue.createApp({
     data() {
         return {
             cart:0,
+            cartEmpty: true,
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
             inStock: true,
@@ -10,6 +11,23 @@ const app = Vue.createApp({
               { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
               { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
             ]
+        }
+    },
+    methods: {
+        addToCart(){
+            this.cart += 1;
+            this.cartEmpty = false
+        },
+
+        removeFromCart(){
+            if (this.cart > 0) {
+                this.cart -= 1;
+                if (this.cart == 0) this.cartEmpty = true
+            }
+        },
+
+        changeImage(image){
+            this.image = image
         }
     }
 })
